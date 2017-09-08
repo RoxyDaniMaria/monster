544d9b891397396bb678e3b3c05d86d2ffe8028d
Maria Mata <mariajo.mata@gmail.com>
2017-09-08 12:09:05 -0600

    trying to get things to show up

--- app/main/monstermain.js
+++ app/main/monstermain.js
@@ -4,22 +4,23 @@
   .module('app')
   .component('monstermain', {
 
-    controller: function($http) {
+    controller: function() {
       const vm = this
       vm.$onInit=function(){
-        // const url = 'https://young-bastion-40394.herokuapp.com/api'
-        // $http.get(url + '/messages').then(function(messages){
-        //   vm.messages = messages.data._embedded.messages
-        //   vm.formstatuses ={
-        //     view:true
-        //   }
-        //   vm.formsubmit = {
-        //     subject : "subject",
-        //     body : "body"
-        //   }
-        // })
-      }
+        console.log('working');
+      //   const url = 'https://infinite-garden-98938.herokuapp.com/api/v1/monsters'
+      //   $http.get(url + '/messages').then(function(messages) {
+      //     vm.messages = messages.data._embedded.messages // monsters info
+      //     vm.formstatuses = {
+      //       view: true
+      //     }
+      //     vm.formsubmit = {
+      //       subject : "subject",
+      //       body : "body"
+      //     }
+      //   })
+      // }
     },
-    templateUrl: 'app/monstermain.html'
+    templateUrl: 'app/main/monstermain.html'
   })
 }());
---
+++ app/menu/monstermenu.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+  angular
+  .module('app')
+  .component('monstermenu', {
+
+    controller: function() {
+      const vm = this
+      vm.$onInit=function(){
+        console.log('working');
+      //   const url = 'https://infinite-garden-98938.herokuapp.com/api/v1/monsters'
+      //   $http.get(url + '/messages').then(function(messages) {
+      //     vm.messages = messages.data._embedded.messages // monsters info
+      //     vm.formstatuses = {
+      //       view: true
+      //     }
+      //     vm.formsubmit = {
+      //       subject : "subject",
+      //       body : "body"
+      //     }
+      //   })
+      // }
+    },
+    templateUrl: 'app/main/monstermenu.html'
+  })
+}());
